d6fb650e743137e6a371a393b844bcdbcde0644d
jasonchilcott <jasonchilcottcode@gmail.com>
2021-01-11 20:13:19 -0500

    finished command line scripts section

--- exercises/ex1.js
+++ exercises/ex1.js
@@ -1,17 +1,37 @@
 #!/usr/bin/env node
 
 "use strict";
-const path = require("path")
+const util = require("util");
+const path = require("path");
+const fs = require("fs");
+const getStdin = require("get-stdin");
 const args = require("minimist")(process.argv.slice(2), {
-  boolean: [ "help" ],
+  boolean: [ "help", "in" ],
   string: [ "file" ]
 } );
 
+const BASE_PATH = path.resolve(
+  process.env.BASE_PATH || __dirname
+)
+
+// if (process.env.HELLO) {
+//   console.log(process.env.HELLO);
+// }
+
 if (args.help) {
   printHelp();
-} else if (args.file) {
-  
-  console.log(filepath);
+} else if (args.in || 
+  args._.includes("-")) {
+  getStdin().then(processFile).catch(error);
+}else if (args.file) {
+  fs.readFile(path.join(BASE_PATH, args.file), function onContents(err, contents){
+    if (err) {
+      error(err.toString());
+    } else {
+      processFile(contents.toString());
+    }
+  });
+
 } else {
   error("Incorrect Usage.", true)
 }
@@ -37,12 +57,24 @@ function error(msg,includeHelp = false) {
     printHelp();
   }
 }
-
+function processFile(contents) {
+  contents = contents.toUpperCase();
+  process.stdout.write(contents)
+//   fs.readFile(filePath, function onContents(err, contents){
+//     if (err) {
+//       error(err.toString());
+//     } else {
+//       contents = contents.toString().toUpperCase();
+//     process.stdout.write(contents)
+//     }
+//   });
+}
 function printHelp() {
   console.log("ex1 usage:");
   console.log("  ex1.js --file={FILENAME}");
   console.log("");
   console.log("--help                print this help");
   console.log("--file={FILENAME}     process the file");
+  console.log("--in, -               process stdin");
   console.log("");
 }
