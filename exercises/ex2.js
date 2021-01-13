6ad84e8ee506819b0cb87beb48b19d6e0cc27a4f
jasonchilcott <jasonchilcottcode@gmail.com>
2021-01-12 19:53:02 -0500

    completed ex2 with compress and uncompress

--- exercises/ex2.js
+++ exercises/ex2.js
@@ -5,15 +5,20 @@ const util = require("util");
 const path = require("path");
 const fs = require("fs");
 const Transform = require("stream").Transform;
+const zlib = require("zlib");
+
 // const getStdin = require("get-stdin");
+
 const args = require("minimist")(process.argv.slice(2), {
-  boolean: [ "help", "in" ],
+  boolean: [ "help", "in", "out", "compress", "uncompress" ],
   string: [ "file" ]
 } );
 
 const BASE_PATH = path.resolve(
   process.env.BASE_PATH || __dirname
 )
+
+let OUTFILE = path.join(BASE_PATH, "out.txt")
 
 
 
@@ -44,16 +49,34 @@ function error(msg,includeHelp = false) {
 function processFile(inStream) {
   let outStream = inStream;
 
+  if (args.uncompress) {
+    let gunzipStream = zlib.createGunzip();
+    outStream = outStream.pipe(gunzipStream);
+  }
+
   let upperStream = new Transform({
     transform(chunk, enc, cb) {
       this.push(chunk.toString().toUpperCase());
-      setTimeout(cb, 500);
       
+      cb();
     }
   })
   outStream = outStream.pipe(upperStream);
+
+  if (args.compress) {
+    let gzipStream = zlib.createGzip();
+    outStream = outStream.pipe(gzipStream);
+    OUTFILE = `${OUTFILE}.gz`;
+  }
   
-  const targetStream = process.stdout;
+  let targetStream;
+  if (args.out) {
+    targetStream = process.stdout;
+
+  } else {
+    targetStream = fs.createWriteStream(OUTFILE)
+  }
+
   outStream.pipe(targetStream);
   //contents = contents.toUpperCase();
   //process.stdout.write(contents)
@@ -61,11 +84,14 @@ function processFile(inStream) {
 
 
 function printHelp() {
-  console.log("ex1 usage:");
-  console.log("  ex1.js --file={FILENAME}");
+  console.log("ex2 usage:");
+  console.log("  ex2.js --file={FILENAME}");
   console.log("");
   console.log("--help                print this help");
   console.log("--file={FILENAME}     process the file");
   console.log("--in, -               process stdin");
+  console.log("--out                 print to stdout");
+  console.log("--compress            gzip the output");
+  console.log("--uncompress          un-gzip the input");
   console.log("");
 }
